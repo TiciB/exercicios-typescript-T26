a944f96ef9e69fb6b756a6e27dc07f2e9605f4d1
TiciB <tebezerra@gmail.com>
2025-04-28 10:39:03 -0300

    Adicionando Exercício soma-dobrada

--- src/implementacao/2-soma-dobrada.ts
+++ src/implementacao/2-soma-dobrada.ts
@@ -1,4 +1,13 @@
-function somaDobrada(umValor: number, outroValor: number) {
-    return null;
+function somaDobrada(num1: number, num2: number): number {
+    
+if (num1 < 1 || num2 < 1) {
+    return -1;
+}
+else if (num1 === num2) {
+    return (num1 + num2)*2;
+}
+else {
+    return num1 + num2;
+}
 }
 module.exports = somaDobrada;
