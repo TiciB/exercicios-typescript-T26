0ac3210d244aab29bd4ba46403a439f41faf3bcd
TiciB <tebezerra@gmail.com>
2025-04-28 14:32:43 -0300

    Adicionanado exercício Contador de Ocorrência

--- src/implementacao/4-contador-de-ocorrencia.ts
+++ src/implementacao/4-contador-de-ocorrencia.ts
@@ -1,4 +1,4 @@
-function contadorDeOcorrencia(texto: string, letra: string) {
-    return null;
+function contadorDeOcorrencia(texto: string, letra: string): number {
+    return texto.split(letra).length -1;
 }
 module.exports = contadorDeOcorrencia;
