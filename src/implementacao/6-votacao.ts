0d3ffd11efccc29f048303c88ca361d5f66fd3e8
TiciB <tebezerra@gmail.com>
2025-04-28 15:14:41 -0300

    Adicionando Exercício Apura votação

--- src/implementacao/6-votacao.ts
+++ src/implementacao/6-votacao.ts
@@ -1,4 +1,26 @@
-function apuraVotacao(votos: Array<number>) {
-    return null;
+function apuraVotacao(votos: Array<number>): string {
+    let voto1 = 0
+    let voto2 = 0
+    let voto3 = 0
+
+    for(let voto of votos){
+        if (voto === 1){
+            voto1++;
+        }else if(voto === 2){
+            voto2++;
+        }else if (voto ===3){
+            voto3++;
+        }
+    }
+
+    if(voto1 > voto2 && voto1 > voto3){
+        return "Vencedor: 1";
+    }else if(voto2 > voto1 && voto2 > voto3){
+        return "Vencedor: 2";
+    }else if(voto3 > voto1 && voto3 > voto2){
+        return "Vencedor: 3";
+    }else{
+        return "Empate"
+    }
 }
 module.exports = apuraVotacao;
